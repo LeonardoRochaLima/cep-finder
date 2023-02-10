2c866fe238c3ee790e5f94dc21d73e85d6cd4e27
Leonardo Lima <leonardo.lima@altatech.com.br>
2023-02-10 16:21:41 -0300

    Corrigindo a função que consome a API VIACEP - Agora todos os dados estão sendo escritos corretamente.

--- web/cep/views.py
+++ web/cep/views.py
@@ -5,18 +5,23 @@ from web.cep.models import Cep
 # Create your views here.
 
 def get_cep_data(request, cep):
-    url = f'https://viacep.com.br/ws/{cep}/json/'
-    response = requests.get(url)
-    return render(request, 'cep_data.html', {'data': response.json()})
+    response = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
 
-def get_cep_data(request, cep):
-    url = f'https://viacep.com.br/ws/{cep}/json/'
-    response = requests.get(url)
-    data = response.json()
-    cep_data, created = Cep.objects.get_or_create(cep=data['cep'])
-    cep_data.logradouro = data['logradouro']
-    cep_data.bairro = data['bairro']
-    cep_data.cidade = data['localidade']
-    cep_data.uf = data['uf']
-    cep_data.save()
-    return render(request, 'cep_data.html', {'data': data})
+    if response.status_code == 200:
+        data = response.json()
+        cep_data = Cep(
+            cep=data['cep'],
+            logradouro=data['logradouro'],
+            complemento=data.get('complemento', ''),
+            bairro=data['bairro'],
+            localidade=data['localidade'],
+            uf=data['uf'],
+            ibge=data.get('ibge', ''),
+            gia=data.get('gia', ''),
+            ddd=data.get('ddd', ''),
+            siafi=data.get('siafi', ''),
+        )
+        cep_data.save()
+        return cep_data
+    else:
+        return None
