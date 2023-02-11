c4ce01d80e188a4d76fd01360c116a6178befef5
Leonardo Lima <leonardo.lima@altatech.com.br>
2023-02-11 12:10:45 -0300

    Corrigindo os erros de inserção de data

--- api/models.py
+++ api/models.py
@@ -16,27 +16,25 @@ class Cep(models.Model):
     ddd = models.CharField(max_length=3, blank=True, null=True)
     siafi = models.CharField(max_length=10, blank=True, null=True)
     lojacorr = models.BooleanField(default=False)
-    created_at = models.CharField(max_length=100)
-    updated_at = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=timezone.now())
+    updated_at = models.DateTimeField(default=timezone.now())
 
     def save(self, *args, **kwargs):
-        self.created_at = timezone.now().strftime("%d-%m-%Y %H:%M:%S")
-        self.updated_at = timezone.now().strftime("%d-%m-%Y %H:%M:%S")
+        self.updated_at = timezone.now()
         return super().save(*args, **kwargs)
     
     def __str__(self):
-        return f"{self.cep} ({self.created_at}) ({self.updated_at})"
+        return f"{self.cep}"
 
 class Estado(models.Model):
     uf = models.CharField(max_length=9, unique=True, primary_key=True)
     nome = models.CharField(max_length=100)
-    created_at = models.CharField(max_length=100)
-    updated_at = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=timezone.now())
+    updated_at = models.DateTimeField(default=timezone.now())
 
     def save(self, *args, **kwargs):
-        self.created_at = timezone.now().strftime("%d-%m-%Y %H:%M:%S")
-        self.updated_at = timezone.now().strftime("%d-%m-%Y %H:%M:%S")
+        self.updated_at = timezone.now()
         return super().save(*args, **kwargs)
     
     def __str__(self):
-        return f"{self.uf} ({self.created_at}) ({self.updated_at})"
+        return f"{self.uf}"
