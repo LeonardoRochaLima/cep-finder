2236b84625ca044fdad04df184de3cbf7f3f17a4
Leonardo Lima <leonardo.lima@altatech.com.br>
2023-02-12 01:54:11 -0300

    Adicionando a execução dos testes no serviço run-script-cepfinder

--- run_script.sh
+++ run_script.sh
@@ -2,3 +2,5 @@
 set -e
 echo "Executando o script de população do banco de dados: python manage.py runscript -v3 populate_estados;"
 python manage.py runscript -v3 populate_estados
+echo "Executando os testes com pytest:"
+pytest
